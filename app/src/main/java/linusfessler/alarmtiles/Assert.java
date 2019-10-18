3f20a9403f451257d1e625f1b6d51e985c5102df
Linus Fessler <linusfessler@gmail.com>
2019-10-18 22:52:50 +0200

    Add asserts

--- app/src/main/java/linusfessler/alarmtiles/Assert.java
+++ app/src/main/java/linusfessler/alarmtiles/Assert.java
@@ -6,9 +6,18 @@ public class Assert {
         throw new UnsupportedOperationException("Do not initialize this static class.");
     }
 
-    public static void isTrue(boolean expression, String message) {
+    public static void isTrue(final boolean expression, final String message) {
         if (!expression) {
             throw new IllegalArgumentException(message);
         }
+    }
+
+    public static void isNotNull(final Object object, final String message) {
+        Assert.isTrue(object != null, message);
+    }
+
+    public static void isNotEmpty(final String string, final String message) {
+        Assert.isNotNull(string, message);
+        Assert.isTrue(!string.equals(""), message);
     }
 }
