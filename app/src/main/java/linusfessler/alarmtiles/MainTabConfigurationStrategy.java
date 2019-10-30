aaaf1916f9a08ec520b4ba58142d427ce19d0255
Linus Fessler <linusfessler@gmail.com>
2019-10-30 21:17:54 +0100

    Add german translation

--- app/src/main/java/linusfessler/alarmtiles/MainTabConfigurationStrategy.java
+++ app/src/main/java/linusfessler/alarmtiles/MainTabConfigurationStrategy.java
@@ -8,11 +8,10 @@ import com.google.android.material.tabs.TabLayoutMediator;
 public class MainTabConfigurationStrategy implements TabLayoutMediator.TabConfigurationStrategy {
 
     @Override
-    public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+    public void onConfigureTab(@NonNull final TabLayout.Tab tab, final int position) {
         switch (position) {
             case 0:
                 tab.setIcon(R.drawable.ic_music_off_24px);
-                tab.setText(R.string.silencer);
                 break;
             case 1:
                 tab.setIcon(R.drawable.ic_timer_24px);
