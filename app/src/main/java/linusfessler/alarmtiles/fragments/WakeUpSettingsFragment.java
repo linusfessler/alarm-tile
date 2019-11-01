5973c1b2fda568abf1a3526ef2fe5e4d7213884b
Linus Fessler <linusfessler@gmail.com>
2019-11-01 12:10:34 +0100

    Make use of binding

--- app/src/main/java/linusfessler/alarmtiles/fragments/WakeUpSettingsFragment.java
+++ app/src/main/java/linusfessler/alarmtiles/fragments/WakeUpSettingsFragment.java
@@ -16,8 +16,6 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.ViewModelProviders;
 
-import com.google.android.material.textview.MaterialTextView;
-
 import linusfessler.alarmtiles.DigitalTimePickerDialog;
 import linusfessler.alarmtiles.R;
 import linusfessler.alarmtiles.databinding.FragmentWakeUpSettingsBinding;
@@ -30,6 +28,7 @@ public class WakeUpSettingsFragment extends Fragment {
 
     private AlarmTile alarmTile;
     private WakeUpSettingsViewModel viewModel;
+    private FragmentWakeUpSettingsBinding binding;
     private boolean is24Hours;
     private TimePickerDialog alarmTimePickerDialog;
     private DigitalTimePickerDialog timerTimePickerDialog;
@@ -62,7 +61,8 @@ public class WakeUpSettingsFragment extends Fragment {
 
         viewModel = ViewModelProviders.of(this).get(WakeUpSettingsViewModel.class);
         viewModel.setAlarmTile(alarmTile);
-        final FragmentWakeUpSettingsBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_wake_up_settings, container, false);
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_wake_up_settings, container, false);
         binding.setViewModel(viewModel);
 
         is24Hours = DateFormat.is24HourFormat(activity);
@@ -74,14 +74,14 @@ public class WakeUpSettingsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull final View view, @Nullable final Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        initAlarmTimePicker(view);
-        initTimerTimePicker(view);
-        initSnoozeTimePicker(view);
-        initVolumeTimerTimePicker(view);
-        initDismissTimerTimePicker(view);
+        initAlarmTimePicker();
+        initTimerTimePicker();
+        initSnoozeTimePicker();
+        initVolumeTimerTimePicker();
+        initDismissTimerTimePicker();
     }
 
-    private void initAlarmTimePicker(final View root) {
+    private void initAlarmTimePicker() {
         final Context context = requireContext();
         final int alarmHour = viewModel.getAlarmHour();
         final int alarmMinute = viewModel.getAlarmMinute();
@@ -91,12 +91,10 @@ public class WakeUpSettingsFragment extends Fragment {
         };
 
         alarmTimePickerDialog = new TimePickerDialog(context, listener, alarmHour, alarmMinute, is24Hours);
-
-        final MaterialTextView alarmTime = root.findViewById(R.id.alarm_time);
-        alarmTime.setOnClickListener(v -> alarmTimePickerDialog.show());
+        binding.alarmTime.setOnClickListener(v -> alarmTimePickerDialog.show());
     }
 
-    private void initTimerTimePicker(final View root) {
+    private void initTimerTimePicker() {
         final Context context = requireContext();
         final int timerHours = viewModel.getTimerHours();
         final int timerMinutes = viewModel.getTimerMinutes();
@@ -106,12 +104,10 @@ public class WakeUpSettingsFragment extends Fragment {
         };
 
         timerTimePickerDialog = new DigitalTimePickerDialog(context, listener, timerHours, timerMinutes, true);
-
-        final MaterialTextView timerDuration = root.findViewById(R.id.timer_duration);
-        timerDuration.setOnClickListener(v -> timerTimePickerDialog.show());
+        binding.timerDuration.setOnClickListener(v -> timerTimePickerDialog.show());
     }
 
-    private void initSnoozeTimePicker(final View root) {
+    private void initSnoozeTimePicker() {
         final Context context = requireContext();
         final int snoozeHours = viewModel.getSnoozeHours();
         final int snoozeMinutes = viewModel.getSnoozeMinutes();
@@ -121,12 +117,10 @@ public class WakeUpSettingsFragment extends Fragment {
         };
 
         snoozeTimePickerDialog = new DigitalTimePickerDialog(context, listener, snoozeHours, snoozeMinutes, true);
-
-        final MaterialTextView snoozeDuration = root.findViewById(R.id.snooze_duration);
-        snoozeDuration.setOnClickListener(v -> snoozeTimePickerDialog.show());
+        binding.snoozeDuration.setOnClickListener(v -> snoozeTimePickerDialog.show());
     }
 
-    private void initVolumeTimerTimePicker(final View root) {
+    private void initVolumeTimerTimePicker() {
         final Context context = requireContext();
         final int volumeHours = viewModel.getVolumeTimerHours();
         final int volumeMinutes = viewModel.getVolumeTimerMinutes();
@@ -136,12 +130,10 @@ public class WakeUpSettingsFragment extends Fragment {
         };
 
         volumeTimerTimePickerDialog = new DigitalTimePickerDialog(context, listener, volumeHours, volumeMinutes, true);
-
-        final MaterialTextView volumeTimerDuration = root.findViewById(R.id.volume_timer_duration);
-        volumeTimerDuration.setOnClickListener(v -> volumeTimerTimePickerDialog.show());
+        binding.volumeTimerDuration.setOnClickListener(v -> volumeTimerTimePickerDialog.show());
     }
 
-    private void initDismissTimerTimePicker(final View root) {
+    private void initDismissTimerTimePicker() {
         final Context context = requireContext();
         final int timerHours = viewModel.getDismissTimerHours();
         final int timerMinutes = viewModel.getDismissTimerMinutes();
@@ -151,9 +143,7 @@ public class WakeUpSettingsFragment extends Fragment {
         };
 
         dismissTimerTimePickerDialog = new DigitalTimePickerDialog(context, listener, timerHours, timerMinutes, true);
-
-        final MaterialTextView dismissTimerDuration = root.findViewById(R.id.dismiss_timer_duration);
-        dismissTimerDuration.setOnClickListener(v -> dismissTimerTimePickerDialog.show());
+        binding.dismissTimerDuration.setOnClickListener(v -> dismissTimerTimePickerDialog.show());
     }
 
     @Override
