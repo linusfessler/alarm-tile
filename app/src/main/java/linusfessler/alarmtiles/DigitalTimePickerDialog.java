0a4e7f7ae32677b83ad3a7b16faff019eb8f6cc3
Linus Fessler <linusfessler@gmail.com>
2019-11-13 22:19:27 +0100

    Update layout

--- app/src/main/java/linusfessler/alarmtiles/DigitalTimePickerDialog.java
+++ app/src/main/java/linusfessler/alarmtiles/DigitalTimePickerDialog.java
@@ -8,32 +8,45 @@ import androidx.appcompat.app.AlertDialog;
 
 public class DigitalTimePickerDialog {
 
+    private final TimePicker timePicker;
     private final AlertDialog alertDialog;
 
     public DigitalTimePickerDialog(final Context context, final TimePicker.OnTimeChangedListener listener,
                                    final int hour, final int minute, final boolean is24HourView) {
-        final TimePicker timePicker = (TimePicker) LayoutInflater.from(context).inflate(R.layout.time_picker_digital, null);
-        timePicker.setOnTimeChangedListener(listener);
-        timePicker.setHour(hour);
-        timePicker.setMinute(minute);
-        timePicker.setIs24HourView(is24HourView);
+        this.timePicker = (TimePicker) LayoutInflater.from(context).inflate(R.layout.time_picker_digital, null, false);
+        this.timePicker.setOnTimeChangedListener(listener);
+        this.timePicker.setHour(hour);
+        this.timePicker.setMinute(minute);
+        this.timePicker.setIs24HourView(is24HourView);
 
-        alertDialog = new AlertDialog.Builder(context)
-                .setView(timePicker)
+        this.alertDialog = new AlertDialog.Builder(context)
+                .setView(this.timePicker)
                 .setCancelable(true)
                 .create();
     }
 
+    public void setHour(final int hour) {
+        this.timePicker.setHour(hour);
+    }
+
+    public void setMinute(final int minute) {
+        this.timePicker.setMinute(minute);
+    }
+
+    public void setIs24HourView(final boolean is24HourView) {
+        this.timePicker.setIs24HourView(is24HourView);
+    }
+
     public void show() {
-        alertDialog.show();
+        this.alertDialog.show();
     }
 
     public void hide() {
-        alertDialog.hide();
+        this.alertDialog.hide();
     }
 
     public void dismiss() {
-        alertDialog.dismiss();
+        this.alertDialog.dismiss();
     }
 
 }
