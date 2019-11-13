9206a7dee60eeb370f00f75b48ff12671cb86b9e
Linus Fessler <linusfessler@gmail.com>
2019-11-13 22:17:44 +0100

    Improve title summary view

--- app/src/main/java/linusfessler/alarmtiles/views/TitleSummaryView.java
+++ app/src/main/java/linusfessler/alarmtiles/views/TitleSummaryView.java
@@ -16,39 +16,49 @@ import linusfessler.alarmtiles.R;
 
 public class TitleSummaryView extends LinearLayout {
 
+    private MaterialTextView titleTextView;
+    private MaterialTextView summaryTextView;
+
     public TitleSummaryView(@NonNull final Context context) {
         super(context);
-        init(context, null, 0, 0);
+        init(context, null);
     }
 
     public TitleSummaryView(@NonNull final Context context, @Nullable final AttributeSet attrs) {
         super(context, attrs);
-        init(context, attrs, 0, 0);
+        init(context, attrs);
     }
 
     public TitleSummaryView(@NonNull final Context context, @Nullable final AttributeSet attrs, final int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context, attrs, defStyleAttr, 0);
+        init(context, attrs);
     }
 
-    public TitleSummaryView(@NonNull final Context context, @Nullable final AttributeSet attrs, final int defStyleAttr, final int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
-        init(context, attrs, defStyleRes, defStyleRes);
-    }
-
-    public void init(@NonNull final Context context, @Nullable final AttributeSet attrs, final int defStyleAttr, final int defStyleRes) {
+    public void init(@NonNull final Context context, @Nullable final AttributeSet attrs) {
         final LayoutInflater inflater = LayoutInflater.from(context);
         final View root = inflater.inflate(R.layout.title_summary_view, this);
-        final MaterialTextView titleTextView = root.findViewById(R.id.title);
-        final MaterialTextView summaryTextView = root.findViewById(R.id.summary);
+
+        this.titleTextView = root.findViewById(R.id.title);
+        this.summaryTextView = root.findViewById(R.id.summary);
+
+        if (attrs == null) {
+            return;
+        }
 
         final TypedArray styledAttributes = context.obtainStyledAttributes(attrs, R.styleable.TitleSummaryView);
         final String title = styledAttributes.getString(R.styleable.TitleSummaryView_title);
         final String summary = styledAttributes.getString(R.styleable.TitleSummaryView_summary);
         styledAttributes.recycle();
 
-        titleTextView.setText(title);
-        summaryTextView.setText(summary);
+        setTitle(title);
+        setSummary(summary);
     }
 
+    public void setTitle(final String title) {
+        this.titleTextView.setText(title);
+    }
+
+    public void setSummary(final String summary) {
+        this.summaryTextView.setText(summary);
+    }
 }
