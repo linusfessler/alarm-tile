bde5b4ce666edd05330d371eb27617acabdc5339
Linus Fessler <linusfessler@gmail.com>
2019-11-13 22:18:12 +0100

    Improve time formatter

--- app/src/main/java/linusfessler/alarmtiles/TimeFormatter.java
+++ app/src/main/java/linusfessler/alarmtiles/TimeFormatter.java
@@ -4,33 +4,38 @@ import java.util.Locale;
 
 public class TimeFormatter {
 
+    private final String hourString;
+    private final String hoursString;
+    private final String minuteString;
+    private final String minutesString;
+
+    public TimeFormatter(final String hourString, final String hoursString, final String minuteString, final String minutesString) {
+        this.hourString = hourString;
+        this.hoursString = hoursString;
+        this.minuteString = minuteString;
+        this.minutesString = minutesString;
+    }
+
     public String format(final int hours, final int minutes) {
-        String hoursString = null;
-        if (hours > 0) {
-            final String suffix = hours == 1 ? "hour" : "hours";
-            hoursString = String.format(Locale.getDefault(), "%d %s", hours, suffix);
+        if (hours != 0 && minutes == 0) {
+            return this.formatHours(hours);
         }
 
-        String minutesString = null;
-        if (minutes > 0) {
-            final String suffix = minutes == 1 ? "minute" : "minutes";
-            minutesString = String.format(Locale.getDefault(), "%d %s", minutes, suffix);
+        if (hours == 0 && minutes != 0) {
+            return this.formatMinutes(hours);
         }
 
-        if (hoursString == null && minutesString == null) {
-            return "0 hours 0 minutes";
-        }
+        return this.formatHours(hours) + " " + this.formatMinutes(minutes);
+    }
 
-        if (hoursString == null) {
-            return minutesString;
-        }
+    private String formatHours(final int hours) {
+        final String hoursSuffix = hours == 1 ? this.hourString : this.hoursString;
+        return String.format(Locale.getDefault(), "%d%s", hours, hoursSuffix);
+    }
 
-        if (minutesString == null) {
-            return hoursString;
-
-        }
-
-        return hoursString + " " + minutesString;
+    private String formatMinutes(final int minutes) {
+        final String minutesSuffix = minutes == 1 ? this.minuteString : this.minutesString;
+        return String.format(Locale.getDefault(), "%d%s", minutes, minutesSuffix);
     }
 
 }
