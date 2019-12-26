8fec827d05ba159161265296f008821f516a9e18
Linus Fessler <linusfessler@gmail.com>
2019-12-26 23:02:46 +0100

    Add @ToString and @EqualsAndHashCode to model classes

--- app/src/main/java/linusfessler/alarmtiles/alarm/Alarm.java
+++ app/src/main/java/linusfessler/alarmtiles/alarm/Alarm.java
@@ -4,9 +4,13 @@ import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @Data
 @Entity
+@ToString
+@EqualsAndHashCode
 public class Alarm {
 
     @PrimaryKey(autoGenerate = true)
