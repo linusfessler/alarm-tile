1d3714c977b6f560a636f098eadaa398d6876e87
Linus Fessler <linusfessler@gmail.com>
2020-02-23 12:02:47 +0100

    Convert tests to Kotlin

---
+++ app/src/test/java/linusfessler/alarmtiles/TimeFormatterTest.kt
@@ -0,0 +1,203 @@
+package linusfessler.alarmtiles
+
+import linusfessler.alarmtiles.shared.TimeFormatter
+import org.junit.Assert
+import org.junit.Test
+import java.util.concurrent.TimeUnit
+
+class TimeFormatterTest {
+    private val timeFormatter = TimeFormatter()
+
+    @Test
+    fun formatMillisWithPrecisionMilliseconds() {
+        // GIVEN
+        val millis = 123456789
+        val precision = TimeUnit.MILLISECONDS
+        val expected = "34h 17m 36s 78"
+        // WHEN
+        val actual = timeFormatter.format(millis.toLong(), precision)
+        // THEN
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun formatMillisWithPrecisionSeconds() {
+        // GIVEN
+        val millis = 123456789
+        val precision = TimeUnit.SECONDS
+        val expected = "34h 17m 36s"
+        // WHEN
+        val actual = timeFormatter.format(millis.toLong(), precision)
+        // THEN
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun formatMillisWithPrecisionMinutes() {
+        // GIVEN
+        val millis = 123456789
+        val precision = TimeUnit.MINUTES
+        val expected = "34h 17m"
+        // WHEN
+        val actual = timeFormatter.format(millis.toLong(), precision)
+        // THEN
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun formatMillisWithPrecisionHours() {
+        // GIVEN
+        val millis = 123456789
+        val precision = TimeUnit.HOURS
+        val expected = "34h"
+        // WHEN
+        val actual = timeFormatter.format(millis.toLong(), precision)
+        // THEN
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun formatHoursMinutesSecondsMillis() {
+        // GIVEN
+        val hours = 4
+        val minutes = 15
+        val seconds = 30
+        val millis = 450
+        val expected = "4h 15m 30s 45"
+        // WHEN
+        val actual = timeFormatter.format(hours.toLong(), minutes.toLong(), seconds.toLong(), millis.toLong())
+        // THEN
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun formatHoursMinutesSeconds() {
+        // GIVEN
+        val hours = 4
+        val minutes = 15
+        val seconds = 30
+        val expected = "4h 15m 30s"
+        // WHEN
+        val actual = timeFormatter.format(hours.toLong(), minutes.toLong(), seconds.toLong())
+        // THEN
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun formatHoursMinutes() {
+        // GIVEN
+        val hours = 4
+        val minutes = 15
+        val expected = "4h 15m"
+        // WHEN
+        val actual = timeFormatter.format(hours.toLong(), minutes.toLong())
+        // THEN
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun formatHours() {
+        // GIVEN
+        val hours = 4
+        val minutes = 0
+        val seconds = 0
+        val millis = 0
+        val expected = "4h 0m 0s 00"
+        // WHEN
+        val actual = timeFormatter.format(hours.toLong(), minutes.toLong(), seconds.toLong(), millis.toLong())
+        // THEN
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun formatMinutes() {
+        // GIVEN
+        val hours = 0
+        val minutes = 15
+        val seconds = 0
+        val millis = 0
+        val expected = "15m 0s 00"
+        // WHEN
+        val actual = timeFormatter.format(hours.toLong(), minutes.toLong(), seconds.toLong(), millis.toLong())
+        // THEN
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun formatSeconds() {
+        // GIVEN
+        val hours = 0
+        val minutes = 0
+        val seconds = 30
+        val millis = 0
+        val expected = "30s 00"
+        // WHEN
+        val actual = timeFormatter.format(hours.toLong(), minutes.toLong(), seconds.toLong(), millis.toLong())
+        // THEN
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun formatMillis() {
+        // GIVEN
+        val hours = 0
+        val minutes = 0
+        val seconds = 0
+        val millis = 450
+        val expected = "45"
+        // WHEN
+        val actual = timeFormatter.format(hours.toLong(), minutes.toLong(), seconds.toLong(), millis.toLong())
+        // THEN
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun formatZeroMillis() {
+        // GIVEN
+        val hours = 0
+        val minutes = 0
+        val seconds = 0
+        val millis = 0
+        val expected = "00"
+        // WHEN
+        val actual = timeFormatter.format(hours.toLong(), minutes.toLong(), seconds.toLong(), millis.toLong())
+        // THEN
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun formatZeroSeconds() {
+        // GIVEN
+        val hours = 0
+        val minutes = 0
+        val seconds = 0
+        val expected = "0s"
+        // WHEN
+        val actual = timeFormatter.format(hours.toLong(), minutes.toLong(), seconds.toLong())
+        // THEN
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun formatZeroMinutes() {
+        // GIVEN
+        val hours = 0
+        val minutes = 0
+        val expected = "0m"
+        // WHEN
+        val actual = timeFormatter.format(hours.toLong(), minutes.toLong())
+        // THEN
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun formatZeroHours() {
+        // GIVEN
+        val hours = 0
+        val expected = "0h"
+        // WHEN
+        val actual = timeFormatter.formatHours(hours.toLong())
+        // THEN
+        Assert.assertEquals(expected, actual)
+    }
+}
---
+++ app/src/test/java/linusfessler/alarmtiles/TimeOfDayFormatterTest.kt
@@ -0,0 +1,106 @@
+package linusfessler.alarmtiles
+
+import linusfessler.alarmtiles.shared.TimeOfDayFormatter
+import org.junit.Assert
+import org.junit.Test
+
+class TimeOfDayFormatterTest {
+    private val timeOfDayFormatter12 = TimeOfDayFormatter(false)
+    private val timeOfDayFormatter24 = TimeOfDayFormatter(true)
+
+    @Test
+    fun format24HoursFormatAm() {
+        // GIVEN
+        val hourOfDay = 4
+        val minutesOfHour = 45
+        val expected = "4:45"
+        // WHEN
+        val actual = timeOfDayFormatter24.format(hourOfDay, minutesOfHour)
+        // THEN
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun format24HoursFormatPm() {
+        // GIVEN
+        val hourOfDay = 16
+        val minutesOfHour = 45
+        val expected = "16:45"
+        // WHEN
+        val actual = timeOfDayFormatter24.format(hourOfDay, minutesOfHour)
+        // THEN
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun format12HoursFormatAm() {
+        // GIVEN
+        val hourOfDay = 4
+        val minutesOfHour = 45
+        val expected = "4:45 AM"
+        // WHEN
+        val actual = timeOfDayFormatter12.format(hourOfDay, minutesOfHour)
+        // THEN
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun format12HoursFormatPm() {
+        // GIVEN
+        val hourOfDay = 16
+        val minutesOfHour = 45
+        val expected = "4:45 PM"
+        // WHEN
+        val actual = timeOfDayFormatter12.format(hourOfDay, minutesOfHour)
+        // THEN
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun format24HoursFormatPreviousDay() {
+        // GIVEN
+        val hourOfDay = -1
+        val minutesOfHour = -1
+        val expected = "22:59"
+        // WHEN
+        val actual = timeOfDayFormatter24.format(hourOfDay, minutesOfHour)
+        // THEN
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun format24HoursFormatNextDay() {
+        // GIVEN
+        val hourOfDay = 25
+        val minutesOfHour = 1
+        val expected = "1:01"
+        // WHEN
+        val actual = timeOfDayFormatter24.format(hourOfDay, minutesOfHour)
+        // THEN
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun format12HoursFormatMidnight() {
+        // GIVEN
+        val hourOfDay = 0
+        val minutesOfHour = 0
+        val expected = "12:00 AM"
+        // WHEN
+        val actual = timeOfDayFormatter12.format(hourOfDay, minutesOfHour)
+        // THEN
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun format12HoursFormatMidday() {
+        // GIVEN
+        val hourOfDay = 12
+        val minutesOfHour = 0
+        val expected = "12:00 PM"
+        // WHEN
+        val actual = timeOfDayFormatter12.format(hourOfDay, minutesOfHour)
+        // THEN
+        Assert.assertEquals(expected, actual)
+    }
+}
